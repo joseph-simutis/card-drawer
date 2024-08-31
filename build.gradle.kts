42048eb28f0c5331c60ed54799e229c0a1aa0e04
Joseph Simutis <js8555240@gmail.com>
2024-08-31 10:21:03 -0400

    v1.1.0-pre.1
    This adds a new tag system, where you can filter the deck without needing to use two different deck files.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "io.github.josephsimutis"
-version = "1.0.0"
+version = "1.1.0-pre.1"
 
 repositories {
     mavenCentral()
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,22 +6,57 @@ import com.github.ajalt.clikt.parameters.types.file
 import com.github.ajalt.clikt.parameters.types.float
 import com.github.ajalt.clikt.parameters.types.int
 
+private fun <E> List<E>.containsAny(second: ArrayList<E>): Boolean {
+    forEach { element ->
+        if (second.contains(element)) return true
+    }
+    return false
+}
+
 class CardDrawer() : CliktCommand() {
     val deckFile by option(help="The location of the deck file").file(mustExist = true).required()
     val count by option(help="The number of cards to draw").int().prompt()
     val delay by option(help="The delay between each card being drawn in seconds").float().default(0f)
-    val removeFromDeck by option(help="Enables the removal of the drawn cards from the deck file").counted()
+    // Make sure to re-add removeFromDeck at some point
+    //val removeFromDeck by option(help="Enables the removal of the drawn cards from the deck file").counted()
+    val tag by option(help="The tags used to filter the deck").multiple()
 
     override fun run() {
-        val deck = ArrayList(deckFile.readLines())
+        // In deck, the first array in the pair is the tags, and the second is the card.
+        val deck = ArrayList<Pair<Array<String>, String>>()
+        val currentTags = ArrayList<String>()
+        deckFile.forEachLine { line ->
+            if (line.startsWith('+')) {
+                currentTags.addAll(line.removePrefix("+").replace(" ", "").split(',').toSet())
+            } else if (line.startsWith('-')) {
+                currentTags.removeAll(line.removePrefix("-").replace(" ", "").split(',').toSet())
+            } else {
+                deck += Pair(currentTags.toTypedArray(), line)
+            }
+        }
+        // In filteredTags, the first arraylist is that tags that are needed, and the second arraylist is the ones that are not allowed.
+        val filteredTags = Pair<ArrayList<String>, ArrayList<String>>(ArrayList(), ArrayList())
+        tag.forEach {
+            if (it.startsWith('-')) {
+                filteredTags.second += it.removePrefix("-")
+            } else {
+                filteredTags.first += it.removePrefix("+")
+            }
+        }
+        val section = ArrayList<String>()
+        deck.forEach { (tags, card) ->
+            if (tags.toList().containsAll(filteredTags.first) && !tags.toList().containsAny(filteredTags.second)) {
+                section.add(card)
+            }
+        }
         for (i in 1..count) {
             Thread.sleep((delay * 1000).toLong())
-            val drawnCard = deck.random()
-            deck.remove(drawnCard)
+            val drawnCard = section.random()
+            section.remove(drawnCard)
             echo(drawnCard)
         }
-        if (removeFromDeck > 0) deckFile.writeText(deck.joinToString("\n"))
+        //if (removeFromDeck > 0) deckFile.writeText(section.joinToString("\n"))
     }
 }
 
-fun main(args: Array<String>) = CardDrawer().versionOption("1.0.0").main(args)
+fun main(args: Array<String>) = CardDrawer().versionOption("1.1.0-pre.1").main(args)
